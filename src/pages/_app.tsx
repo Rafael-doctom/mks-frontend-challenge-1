5ed7b548cdeca55ce30c6b1be23aa6e0b46af83c
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-20 23:16:55 -0300

    add meta tag to the app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import GlobalStyle from '../styles/globals'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>MKS Sistemas</title>
+        <meta name="title" content="MKS Sistemas" />
+        <meta name="description" content="Desafio frontend proposto pela empresa MKS Desenvolvimento de Sistemas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </>
