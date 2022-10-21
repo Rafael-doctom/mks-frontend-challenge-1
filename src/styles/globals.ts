d9f6483f18173c31e7c2e23822170cd10192bedb
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-21 01:21:53 -0300

    add Montserrat font to the GlobalStyle

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -5,10 +5,8 @@ export default createGlobalStyle`
     margin:0;
     padding:0;
     box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
   }
 
-  body {
-
-    background-color:red
-  }
+ 
 `
