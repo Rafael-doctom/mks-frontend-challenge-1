449055fae5f7578bd191601f362a8ea7aef5c8c3
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-21 01:19:33 -0300

    add Montserrat font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="title" content="MKS Sistemas" />
         <meta name="description" content="Desafio frontend proposto pela empresa MKS Desenvolvimento de Sistemas" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet"></link>
       </Head>
       <Component {...pageProps} />
       <GlobalStyle />
