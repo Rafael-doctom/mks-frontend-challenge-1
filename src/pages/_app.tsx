7251ee76d675444b9a6e63b705d6d331106fea8b
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-21 14:17:12 -0300

    add _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import GlobalStyle from '../styles/globals'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>MKS Sistemas</title>
-        <meta name="title" content="MKS Sistemas" />
-        <meta name="description" content="Desafio frontend proposto pela empresa MKS Desenvolvimento de Sistemas" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet"></link>
-      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </>
