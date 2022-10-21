f05576c5e29f0d2185ad3d3f7014a553485f5f4a
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-21 16:22:29 -0300

    add ModalCart component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Products from '../components/Products'
+import Footer from '../components/Footer'
+import ModalCart from '../components/ModalCart'
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div>
 
       <Header />
-  
-      <main >
-      
-      </main>
-
-      <footer >
-        
-      </footer>
+      <Products />
+      <Footer />
+      <ModalCart />
     </div>
   )
 }
