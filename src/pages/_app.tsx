5004e0c6bceb2066a9951c2104054a58d95b883e
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-22 12:04:57 -0300

    create a store a wrap the app with a provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import GlobalStyle from '../styles/globals'
 import type { AppProps } from 'next/app'
+import { store } from '../store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </Provider>
   )
 }
 
