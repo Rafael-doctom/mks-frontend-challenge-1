bdb2f0a94f855a1ac089537d47bb79288a8a51fc
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-22 19:56:18 -0300

    add media query to the Products component

--- src/components/Products/styles.ts
+++ src/components/Products/styles.ts
@@ -2,10 +2,33 @@ import styled from 'styled-components';
 
 export const ProductsContainer = styled.main`
 
+    
     height:100vh;
     display:grid;
     align-content: center;
     justify-content: center;
     grid-template-columns: auto auto auto auto;
     gap:30px 22px;
+    padding:20px 0;
+
+    /* desktop */
+    @media (max-width:1020px) {
+
+        grid-template-columns: auto auto auto;
+        height:100%;
+        
+    }
+
+    /* tablet */
+    @media (max-width:800px) {
+
+        grid-template-columns: auto auto;
+
+    }
+
+    /* mobile */
+    @media (max-width:570px) {
+
+        grid-template-columns: auto;
+    }
 `
