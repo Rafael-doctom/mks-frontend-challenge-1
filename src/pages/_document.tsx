7487a01bb58d19a4d9181338317893efa2b61345
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-22 12:05:32 -0300

    remove tittle from the document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <title>MKS Sistemas</title>
-                <meta name="title" content="MKS Sistemas" />
-                <meta name="description" content="Desafio frontend proposto pela empresa MKS Desenvolvimento de Sistemas" />
-                <link rel="icon" href="/favicon.ico" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet"></link>
             </Head>
             <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
-import type { NextPage } from 'next'
 import { InferGetStaticPropsType } from 'next'
 import Header from '../components/Header'
 import Products from '../components/Products'
 import Footer from '../components/Footer'
 import Modal from '../components/Modal'
 import Product from '../types/Product'
+import Head from 'next/head'
 
 const Home = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
+      <Head>
+        <title>MKS Sistemas</title>
+        <meta name="title" content="MKS Sistemas" />
+        <meta name="description" content="Desafio frontend proposto pela empresa MKS Desenvolvimento de Sistemas" />
+        <link rel="icon" href="/favicon.ico" />        
+      </Head>
       <Header />
       <Products products={products}/>
       <Footer />
