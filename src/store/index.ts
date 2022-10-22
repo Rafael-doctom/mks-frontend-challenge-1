a5cb71ea05a37a2a55501c0931d5fc5e751cb6f1
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-22 13:29:35 -0300

    add modal reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import ModalReducer from '../slices/modalSlice'
 
 export const store = configureStore({
 
-    reducer:{},
+    reducer:{
+        modal:ModalReducer
+    },
 });
 
 
