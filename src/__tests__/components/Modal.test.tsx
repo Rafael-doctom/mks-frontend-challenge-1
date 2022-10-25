619707ab7cc11d9d6e166b33321454638dbaf544
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-25 16:17:09 -0300

    add tests to the Modal component

---
+++ src/__tests__/components/Modal.test.tsx
@@ -0,0 +1,53 @@
+// utilities
+import Modal from '../../components/Modal'
+
+import { renderWithRedux } from '../../helpers/testHelpers'
+import ModalItem from '../../types/ModalItem'
+
+describe('Modal component', () => {
+
+    it('should show the correct modal products', () => {
+
+        const TEST_PRODUCTS:ModalItem[] = [
+
+            {
+                id:1,
+                name:'Air Pods',
+                description:'Lorem ipsum 2', 
+                photo:'https://mks-sistemas.nyc3.digitaloceanspaces.com/products/airpods.webp',
+                price:1200.00, 
+                quantity: 3
+            }
+        ]
+
+        const { getByText } = renderWithRedux(<Modal />, {
+            preloadedState: {
+              modal: {
+                products:TEST_PRODUCTS
+              }
+            } as any
+        })
+        
+        getByText(/air pods/i)
+        getByText('3')
+        getByText('R$1200')
+        getByText('R$3600')
+        getByText(/finalizar compra/i)
+    })
+
+    it('should show the empty modal phrase if the modal is empty', () => {
+
+        const TEST_PRODUCTS:ModalItem[] = []
+
+        const { getByText, debug } = renderWithRedux(<Modal />, {
+            preloadedState: {
+              modal: {
+                products:TEST_PRODUCTS
+              }
+            } as any
+        })
+        
+        getByText(/seu carrinho está vazio/i)
+        getByText(/adicione itens/i)
+    })
+})
