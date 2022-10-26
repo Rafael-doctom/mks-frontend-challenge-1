c7a1b97b4cd6430bc4a7973066759023aeff4dce
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-25 22:23:56 -0300

    add Modal tests

--- src/__tests__/components/Modal.test.tsx
+++ src/__tests__/components/Modal.test.tsx
@@ -1,3 +1,5 @@
+import userEvent from '@testing-library/user-event'
+
 // components
 import Modal from '../../components/Modal'
 
@@ -50,5 +52,98 @@ describe('Modal component', () => {
         
         getByText(/seu carrinho está vazio/i)
         getByText(/adicione itens/i)
+    })
+
+    it('should decrease product quantity on decrease click', async () => {
+
+        const TEST_PRODUCTS:IModalItem[]= [{
+
+            id:1,
+            name:'Air Pods',
+            description:'Lorem ipsum 2', 
+            photo:'https://mks-sistemas.nyc3.digitaloceanspaces.com/products/airpods.webp',
+            price:1200.00, 
+            quantity: 3
+        }]
+     
+        const { getByText, getByLabelText, debug } = renderWithRedux(<Modal />,
+            {
+                preloadedState: {
+
+                    modal: {
+
+                        products: TEST_PRODUCTS
+                    }
+                } as any
+            }
+        )
+
+        const decreaseButton = getByLabelText(/decrease quantity of air pods/i)
+        await userEvent.click(decreaseButton)
+
+        getByText('2')
+        
+    })
+
+    it('should increase product quantity on increase click', async () => {
+
+        const TEST_PRODUCTS:IModalItem[]= [{
+
+            id:1,
+            name:'Air Pods',
+            description:'Lorem ipsum 2', 
+            photo:'https://mks-sistemas.nyc3.digitaloceanspaces.com/products/airpods.webp',
+            price:1200.00, 
+            quantity: 3
+        }]
+     
+        const { getByText, getByLabelText, debug } = renderWithRedux(<Modal />,
+            {
+                preloadedState: {
+
+                    modal: {
+
+                        products: TEST_PRODUCTS
+                    }
+                } as any
+            }
+        )
+
+        const increaseButton = getByLabelText(/increase quantity of air pods/i)
+        await userEvent.click(increaseButton)
+
+        getByText('4')   
+    })
+
+    it('should remove product on remove click', async () => {
+
+        const TEST_PRODUCTS:IModalItem[]= [{
+
+            id:1,
+            name:'Air Pods',
+            description:'Lorem ipsum 2', 
+            photo:'https://mks-sistemas.nyc3.digitaloceanspaces.com/products/airpods.webp',
+            price:1200.00, 
+            quantity: 3
+        }]
+     
+        const { getByText, getByLabelText, debug, queryByText } = renderWithRedux(<Modal />,
+            {
+                preloadedState: {
+
+                    modal: {
+
+                        products: TEST_PRODUCTS
+                    }
+                } as any
+            }
+        )
+
+        const removeButton = getByLabelText(/remove air pods/i)
+        await userEvent.click(removeButton)
+
+        expect(queryByText(/air pods/i)).toBeNull()
+        getByText(/seu carrinho está vazio/i)  
+        getByText(/adicione itens/i)  
     })
 })
