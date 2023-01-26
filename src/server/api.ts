bec8a2e5d835a5293e015aff92abbab22697c0cb
Beatriz Sant'Anna <zirtaebn@gmail.com>
2023-01-26 00:20:41 -0300

    update api url

--- src/server/api.ts
+++ src/server/api.ts
@@ -5,7 +5,8 @@ import Product from '../types/Product'
 
 const getProducts = async (): Promise<Product[]> => {
 
-    const { data } = await axios.get('https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC')
+    const { data } = await axios.get('https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC')
+                                      
     return data.products
 }
 
